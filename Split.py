f86c258e92891ff67f4c6f26644e9f1af309de0d
Hyodan <43164773+EvilEdge@users.noreply.github.com>
2019-11-07 13:18:06 +0900

    Update Split.py
    Clean repeat data. Complete

--- Split.py
+++ Split.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import numpy as np
 import csv
-steam = pd.read_csv('D:\steamk.csv', header = None,names=['user', 'game', 'purchase_play', 'hrs'])
+steam = pd.read_csv('C:\Python\steam.csv',header=None,names=['user', 'game', 'purchase_play', 'hrs'])
 
-#clean own/play
-
-#split purchase
+#split purchase and calculate time
 steam['purchase']=1
+steam['hrs']=steam['hrs']-steam['purchase']
 
 #split play
 steam['play']=np.where(steam['purchase_play']=='play',1,0)
 
+#clean own/play
+clean_steam=steam.drop_duplicates(subset=['user','game'],keep='last')
 
+print(clean_steam)
 
-print(steam)
