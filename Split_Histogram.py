2b8242e7e7d175cd0c1a68e56277ea06dce9a768
Hyodan <43164773+EvilEdge@users.noreply.github.com>
2019-11-10 01:12:57 +0900

    Fix Histogram

--- Split_Histogram.py
+++ Split_Histogram.py
@@ -3,8 +3,12 @@ import numpy as np
 import pathlib
 from plotnine import *
 from plotnine.data import *
+from pandas.api.types import CategoricalDtype
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-locationUsersFile = pathlib.Path(r'data/steam_user_test.csv')
+
+locationUsersFile = pathlib.Path(r'data/steam_user_train.csv')
 steam = pd.read_csv(locationUsersFile,header=0)
 print(steam)
 #Split purchase and calculate time
@@ -23,20 +27,29 @@ clean_steam.to_csv(r'data/purchase_play.csv',index=None)
 #Most played game - hrs
 played=clean_steam.groupby(['game_name'])
 phrs=played.agg({'hours':np.sum})
-
+phrs=phrs.round(1)
 #Most played game - users
 played=played['game_name'].count().reset_index(name="alusers")
 
 #Merge user - hrs
 mp=pd.merge(phrs,played,on='game_name')
 most=(mp.sort_values(by='alusers',ascending=False)).head(20)
+Eb=pd.Categorical(most,ordered=True)
 
 #Histogram
 print(most)
-print(ggplot(most, aes(x = 'game_name', y = 'alusers', fill = 'hours')) +
-      geom_bar(stat = "identity") +
-      theme(axis_text_x=element_text(angle=90,hjust=0.5,vjust=1)) +
-      labs(title = "Top 20 games with the most users", x = "Game", y = "Number of users"))
+sns.set(style="darkgrid")
+sns.barplot(x='alusers',y='game_name',hue='hours',alpha=0.9,data=most,dodge=False)
+plt.title('Top 20 games with the most users')
+plt.ylabel('Game', fontsize=12)
+plt.xlabel('Top 20 games with the most users', fontsize=12)
+plt.show()
+
+#print(ggplot(Eb, aes(x = 'game_name', y = 'alusers', fill = 'hours')) +
+#      geom_bar(stat = "identity") +
+#     theme(axis_text_x=element_text(angle=90,hjust=0.5,vjust=1)) +
+#     labs(title = "Top 20 games with the most users", x = "Game", y = "Number of users"))
+
 
 
 
