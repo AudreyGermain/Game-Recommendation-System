8884d234ba29b5e43c21493660a5e7978b67ddae
Hyodan <43164773+EvilEdge@users.noreply.github.com>
2019-11-18 07:52:37 +0900

    modifty

--- EM Rating.py
+++ EM Rating.py
@@ -1,26 +1,66 @@
 import pandas as pd
 import pathlib
+import numpy as np
 locationUsersFile = pathlib.Path(r'data/purchase_play.csv')
-steam = pd.read_csv(locationUsersFile,header=0)
+data = pd.read_csv(locationUsersFile,header=0)
 
-# create a rating based on time played
-game_hrs_density <- function(GAME, nclass, print_vals = TRUE){
-    # subsetting data
-    game_data <- subset(steam_clean, game1 == GAME & hrs > 2)
-game_data$loghrs <- log(game_data$hrs)
+def Generate_EM(game,rating,print_vals=TRUE):
+  gdata=data[game,data.hrs>2]
+  gdata['loghrs']=log(gdata['hrs'])
+  mui=np.linespace(min(gdata['loghrs']),max(gdata['loghrs']),length=rating)
+  EM=normalmixEm(gdata['loghrs'].mu=mui,sigma=rep(1,nclass))
 
-# em algorithm
-mu.init <- seq(min(game_data$loghrs), max(game_data$loghrs), length = nclass)
-EM <- normalmixEM(game_data$loghrs, mu = mu.init, sigma=rep(1, nclass))
 
-# print results
-if(print_vals){
-cat(" lambda: ", EM$lambda, "\n mean  : ", EM$mu, "\n sigma : ", EM$sigma, "\n")
-}
+def get_pdf(sample, mu, sigma):
+    res = stats.multivariate_normal(mu, sigma).pdf(sample)
+    return res
 
-# building data frame for plotting
-x <- seq(min(game_data$loghrs), max(game_data$loghrs), 0.01)
-dens <- data.frame(x = x)
-for(k in 1:nclass){
-    dens[,paste0('y', k)] <- EM$lambda[k]*dnorm(x, EM$mu[k], EM$sigma[k])
-}
+
+def get_log(data, k, mu, sigma, gama):
+    res = 0.0
+    for i in range(len(data)):
+        cur = 0.0
+        for j in range(len(k)):
+            cur += gama[j][i] * get_pdf(data[i], mu[j], sigma[j])
+        res += math.log(cur)
+    return res
+
+def EM(data,k,mu,sigma,steps=1000):
+    num_gau = len(k)  
+    num_data = data.shape[0]  
+    gama = np.zeros((num_gau, num_data)) 
+    record = []  
+    for step in range(steps):
+        #gama matrix
+        for i in range(num_gau):
+            for j in range(num_data):
+                gama[i][j] = k[i] * get_pdf(data[j], mu[i], sigma[i]) / \
+                             sum([k[t] * get_pdf(data[j], mu[t], sigma[t]) for t in range(num_gau)])
+        cur_game = get_log(data, k, mu, sigma, gama)  # calculate log
+        record.append(cur_game)
+        # update mu
+        for i in range(num_gau):
+            mu[i] = np.dot(gama[i], data) / np.sum(gama[i])
+        # update sigma
+        for i in range(num_gau):
+            cov = [np.dot((data[t] - mu[i]).reshape(-1, 1), (data[t] - mu[i]).reshape(1, -1)) for t in range(num_data)]
+            cov_sum = np.zeros((2, 2))
+            for j in range(num_data):
+                cov_sum += gama[i][j] * cov[j]
+            sigma[i] = cov_sum / np.sum(gama[i])
+        # update k
+        for i in range(num_gau):
+            k[i] = np.sum(gama[i]) / num_data
+        print('step: {}\t game:{}'.format(step + 1, cur_game))
+    return k, mu, sigma, gama, record
+
+def main():
+    k_init = [0.25, 0.25, 0.25, 0.25]
+    mu_init = [[-0.5, -0.5],
+               [-0.4, 0.4],
+               [0.1, 0.1],
+               [1.2, -1.2]]
+    sigma_init = [[[1, 0.5], [0.5, 1]],
+                  [[0.5, 0], [0, 0.5]],
+                  [[0.3, 0], [0, 0.3]],
+                  [[0.5, -0.1], [-0.1, 0.5]]]
