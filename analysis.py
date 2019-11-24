78053379b9e2ae5f8b44ba74710d091f0812bf47
Audrey Germain <audrey.germain@polymtl.ca>
2019-11-24 16:21:11 +0900

    modify the way training and test datasets are split

--- analysis.py
+++ analysis.py
@@ -3,22 +3,15 @@ import random
 import pathlib
 
 # Get data from CSV
-locationUsersFile = pathlib.Path(r'data/steam-200k.csv')
-dataUsers = read_csv(locationUsersFile, header=None, usecols=[0, 1, 2, 3],
-                     names=["user_id", "game_name", "behavior", "hours"])
+locationUsersFile = pathlib.Path(r'data/purchase_play.csv')
+dataUsers = read_csv(locationUsersFile)
 
-# get list of all users
-userArray = dataUsers["user_id"].unique()
+# get 20% of random elements (combination user-game) for test dataset
+testUsers = dataUsers.sample(frac=0.2, replace=False)
 
-# get number of users and find 20% of random users for test dataset
-userCount = len(userArray)
-testUserCount = round(0.2*userCount)
-testUsers = random.sample(list(userArray), testUserCount)
+# get the remaining elements for training dataset
+trainUsers = dataUsers[~dataUsers.isin(testUsers)].dropna()
 
-# split user in 2 sets and output csv
-criteriaTest = dataUsers['user_id'].isin(testUsers)
-trainDataset = dataUsers[~criteriaTest]
-testDataset = dataUsers[criteriaTest]
-testDataset.to_csv(pathlib.Path(r'data/steam_user_test.csv'), columns=["user_id", "game_name", "behavior", "hours"], index=False)
-trainDataset.to_csv(pathlib.Path(r'data/steam_user_train.csv'), columns=["user_id", "game_name", "behavior", "hours"], index=False)
-
+# output csv
+testUsers.to_csv(pathlib.Path(r'data/steam_user_test.csv'), index=False)
+trainUsers.to_csv(pathlib.Path(r'data/steam_user_train.csv'), index=False)
