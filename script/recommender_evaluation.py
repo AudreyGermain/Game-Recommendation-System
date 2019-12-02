922884feba23d88c4872bcadfcb75b8d059c0a4a
Audrey Germain <audrey.germain@polymtl.ca>
2019-12-02 22:18:53 +0900

    fix path in recommender evaluation script

--- script/recommender_evaluation.py
+++ script/recommender_evaluation.py
@@ -3,29 +3,29 @@ import pathlib
 
 n_recommendation = 20
 
-locationAlgoOutputFile = pathlib.Path(r'data/Content-based-recommender-output.csv')
+locationAlgoOutputFile = pathlib.Path(r'../data/output_data/Content_based_recommender_output.csv')
 dataOutputs = read_csv(locationAlgoOutputFile)
 
-locationTestFile = pathlib.Path(r'data/steam_user_test.csv')
+locationTestFile = pathlib.Path(r'../data/model_data/steam_user_test.csv')
 dataTest = read_csv(locationTestFile)
 
-dataOutputs["numberGames"] = 0
-dataOutputs["numberGamesUserHas"] = 0
+dataOutputs["numberGamesUserHasInTest"] = 0
+dataOutputs["numberRecommendationUserHas"] = 0
 dataOutputs["ratio"] = 0
 
 for i, row in dataOutputs.iterrows():
     userGames = dataTest[dataTest["user_id"] == row["user_id"]]["game_name"].tolist()
-    dataOutputs.at[i, 'numberGames'] = len(userGames)
+    dataOutputs.at[i, 'numberGamesUserHasInTest'] = len(userGames)
     count = 0
     for j in range(1, n_recommendation+1):
         if row[j] in userGames:
             count += 1
-    dataOutputs.at[i, "numberGamesUserHas"] = count
+    dataOutputs.at[i, "numberRecommendationUserHas"] = count
     if len(userGames) != 0:
         dataOutputs.at[i, "ratio"] = float(count/len(userGames))
 
 print(dataOutputs["ratio"].describe(include=[float]))
-print(dataOutputs["numberGamesUserHas"].describe(include=[float]))
-print(dataOutputs["numberGames "].describe(include=[float]))
-dataOutputs.to_csv(pathlib.Path(r'data/Content-based-evaluation.csv'),
-                   columns=["user_id", "ratio", "numberGamesUserHas", "numberGames"], index=False)
+print(dataOutputs["numberRecommendationUserHas"].describe(include=[float]))
+print(dataOutputs["numberGamesUserHasInTest"].describe(include=[float]))
+dataOutputs.to_csv(pathlib.Path(r'../data/evaluation_data/Content-based-evaluation.csv'),
+                   columns=["user_id", "ratio", "numberRecommendationUserHas", "numberGamesUserHasInTest"], index=False)
